1a99d00cce9683826546108130702683aa217132
George Roman <george.roman.99@gmail.com>
2022-02-25 18:37:44 +0200

    Remove temporary script

--- src/index.ts
+++ src/index.ts
@@ -6,44 +6,10 @@ import "@/jobs/index";
 
 import { start } from "@/api/index";
 import { logger } from "@/common/logger";
-import { db } from "./common/db";
-import { addToOrdersUpdateByHashQueue } from "./jobs/orders-update";
-import { config } from "./config/index";
 
 process.on("unhandledRejection", (error) => {
   logger.error("process", `Unhandled rejection: ${error}`);
   process.exit(1);
 });
-
-const foo = async () => {
-  if (!config.master) return;
-  let done = false;
-  let i = 0;
-  while (!done) {
-    console.log(i++);
-    const results = await db.manyOrNone(
-      `
-        update "orders" as "o"
-        set "status" = 'cancelled'
-        from (
-          select "hash" from "orders"
-          where "kind" = 'wyvern-v2'
-            and ("status" = 'valid' or "status" = 'no-balance')
-          limit 100
-        ) "x"
-        where "o"."hash" = "x"."hash"
-        returning "o"."hash"
-      `
-    );
-    await addToOrdersUpdateByHashQueue(
-      results.map(({ hash }) => ({ context: `cancelled-${hash}`, hash }))
-    );
-
-    if (results.length < 100) {
-      done = true;
-    }
-  }
-};
-foo();
 
 start();
