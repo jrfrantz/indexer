d88082e31607b3fb5346be47b1687077109ebf9b
George Roman <george.roman.99@gmail.com>
2022-02-28 09:57:01 +0200

    Disable wyvern v2.2 orders

--- src/api/endpoints/orders/post-orders.ts
+++ src/api/endpoints/orders/post-orders.ts
@@ -3,10 +3,9 @@ import { Request, RouteOptions } from "@hapi/hapi";
 import * as Sdk from "@reservoir0x/sdk";
 import Joi from "joi";
 
-import { wyvernV2OrderFormat, wyvernV23OrderFormat } from "@/api/types";
+import { wyvernV23OrderFormat } from "@/api/types";
 import { logger } from "@/common/logger";
 import { config } from "@/config/index";
-import * as wyvernV2 from "@/orders/wyvern-v2";
 import * as wyvernV23 from "@/orders/wyvern-v2.3";
 
 export const postOrdersOptions: RouteOptions = {
@@ -20,15 +19,9 @@ export const postOrdersOptions: RouteOptions = {
     payload: Joi.object().keys({
       orders: Joi.array().items(
         Joi.object().keys({
-          kind: Joi.string()
-            .lowercase()
-            .valid("wyvern-v2", "wyvern-v2.3")
-            .required(),
+          kind: Joi.string().lowercase().valid("wyvern-v2.3").required(),
           data: Joi.alternatives().conditional("kind", {
-            switch: [
-              { is: "wyvern-v2", then: wyvernV2OrderFormat },
-              { is: "wyvern-v2.3", then: wyvernV23OrderFormat },
-            ],
+            switch: [{ is: "wyvern-v2.3", then: wyvernV23OrderFormat }],
           }),
           attribute: Joi.object({
             collection: Joi.string().required(),
@@ -49,17 +42,9 @@ export const postOrdersOptions: RouteOptions = {
     try {
       const orders = payload.orders as any;
 
-      const validOrderInfosWyvernV2: wyvernV2.OrderInfo[] = [];
       const validOrderInfosWyvernV23: wyvernV23.OrderInfo[] = [];
       for (const { kind, data, attribute } of orders) {
-        if (kind === "wyvern-v2") {
-          try {
-            const order = new Sdk.WyvernV2.Order(config.chainId, data);
-            validOrderInfosWyvernV2.push({ order, attribute });
-          } catch {
-            // Skip any invalid orders
-          }
-        } else if (kind === "wyvern-v2.3") {
+        if (kind === "wyvern-v2.3") {
           try {
             const order = new Sdk.WyvernV23.Order(config.chainId, data);
             validOrderInfosWyvernV23.push({ order, attribute });
@@ -71,28 +56,6 @@ export const postOrdersOptions: RouteOptions = {
 
       let validCount = 0;
       const result: { [hash: string]: string } = {};
-
-      // Handle WyvernV2 orders
-      {
-        const filterResults = await wyvernV2.filterOrders(
-          validOrderInfosWyvernV2
-        );
-        const saveResults = await wyvernV2.saveOrders(
-          filterResults.valid,
-          false
-        );
-
-        for (const { orderInfo, reason } of filterResults.invalid) {
-          result[orderInfo.order.prefixHash()] = reason;
-        }
-        for (const { orderInfo, reason } of saveResults.invalid) {
-          result[orderInfo.order.prefixHash()] = reason;
-        }
-        for (const orderInfo of saveResults.valid) {
-          result[orderInfo.order.prefixHash()] = "Success";
-          validCount++;
-        }
-      }
 
       // Handle WyvernV23 orders
       {
